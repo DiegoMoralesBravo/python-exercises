8028b2f8d3dd781a0f7158c2a7c767b357ebaae9
DiegoMoralesBravo <Diegomorales1359@gmail.com>
2022-03-13 20:02:39 -0600

    Se resuelven problemas

---
+++ excercises/23.py
@@ -0,0 +1,16 @@
+# ### Question 23
+# level 1
+#
+# Question:
+# Write a method which can calculate square value of number
+#
+# Hints:
+# Using the ** operator
+
+class Objeto():
+    def square(self, x):
+        return x**2
+
+
+diego = Objeto()
+print(diego.square(2))
